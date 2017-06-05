a9aea52662355871a856d965b37cb405ab5dd991
makevoid <makevoid@gmail.com>
2017-06-05 11:56:13 +0100

    delete old json files, add some specs around basic calls

--- Gemfile
+++ Gemfile
@@ -3,7 +3,9 @@ source "https://rubygems.org"
 # gemspec
 
 gem "oj"
-gem "digest-sha3"
+# gem "digest-sha3"
+gem "digest-sha3", git: "https://github.com/steakknife/digest-sha3-ruby.git"
+
 
 gem "rlp"
 # gem "ruby-ethereum", git: "https://github.com/cryptape/ruby-ethereum", require: false
--- spec/calls_spec.rb
+++ spec/calls_spec.rb
@@ -2,25 +2,34 @@ require_relative '../ethereum'
 
 ETH = Ethereum::Eth.new
 
-
-
 describe "RPC Calls" do
-  xit "gets block height" do
-    ETH.block_get.should == "123"
+  it "gets block height" do
+    ETH.block_get.should_not be_nil
+    ETH.block_get.should be_a Integer
+    ETH.block_get.should be >= 0
   end
 
-  xit "coinbase" do
+  it "coinbase" do
     coinbase = ETH.coinbase
     coinbase[0..1].should == "0x"
-    # coinbase.size.should == 123
+    coinbase.size.should  == 42
   end
 
-  xit "gets balance" do
-    # need to use it with a maximum balance otherwise the spec will fail
+  it "gets balance" do
     scale = 10 ** 5
-    balance = ETH.balance / scale
-    balance.round.should == 1.6
+    balance = ETH.balance
+    balance[0..1].should == "0x"
+    # TODO: FIXME: return a numeric balance
   end
 
+  specify "get (eth_call)" do
+    # note: this spec requires deployed contracts
+    ETH.get contract: :key_value, method: :get, params: ["foo"]
+  end
+
+  specify "set (eth_sendTransaction)" do
+
+
+  end
 
 end
